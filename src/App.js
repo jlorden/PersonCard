c6932ea6e0097b7ae2a773916eab787006d68ce8
Jlorden <Jamie.lorden@gmail.com>
2020-05-07 12:59:53 -0500

    Personswithfunctionprops

--- src/App.js
+++ src/App.js
@@ -9,35 +9,41 @@ import PersonCard from './Components/PersonCard';
 
 function App() {
   return (
-    <div className="container">
-      <div className="jumbotron">
+    <div>
+      <div>
         <h1>Here are some people</h1>
       </div>
-      <div className="row">
-        <PersonCard 
-          lastName="Doe,"
+      <div>
+        <PersonCard firstName="Jane" lastName="Doe" age={ 45 } hairColor="Black" />
+        <PersonCard firstName="John" lastName="Smith" age={ 88 } hairColor="Brown" />
+        <PersonCard firstName="Millard" lastName="Fillmore" age={ 50 } hairColor="Brown" /> 
+        <PersonCard firstName="Maria" lastName="Smith" age={ 62 } hairColor="Brown" /> 
+
+{/* //Note: we can pass down a string with or without curly braces. */}
+ 
+          {/* lastName="Doe"
           firstName="Jane"
           age={45}
           hairColor="Black"
-        />
-        <PersonCard 
-          lastName="Smith,"
+        /> */}
+        {/* <PersonCard 
+          lastName="Smith"
           firstName="John"
           age={88}
           hairColor="Brown"
         />
         <PersonCard 
-          lastName="Fillmore,"
+          lastName="Fillmore"
           firstName="Millard"
           age={50}
           hairColor="Brown"
         />
         <PersonCard 
-          lastName="Smith,"
+          lastName="Smith"
           firstName="Maria"
           age={62}
           hairColor="Brown"
-        />
+        /> */}
         
       </div>
     </div>
--- src/Components/PersonCard.jsx
+++ src/Components/PersonCard.jsx
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
-class PersonCard extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentAge: this.props.age
-    };
-}
-    birthday = () => {
-        this.state.currentAge < 100 ? this.setState({ currentAge: this.state.currentAge + 1 }) : alert("You have another life");
-}
-    render() {
-        return (
+const PersonCard = props => {
+    return(
         <div>
-            <div>
-            <div>
-                <h2>
-                Name:{ " " }
-                {this.props.firstName} { " " }
-                {this.props.lastName}
-                </h2>
-            </div>
-            <p>Age: {this.state.currentAge}</p>
-            <p>Hair Color: {this.props.hairColor}</p>
-            <button
-                onClick= {this.birthday}
-            >
-                birthday
-            </button>
-            </div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
         </div>
-        );
-    }
+    );
+
+    // birthday = () => {
+    //     this.state.currentAge < 100 ? this.setState({ currentAge: this.state.currentAge + 1 }) : alert("You have another life");
+    // }
+    // render() {
+    //     return (
+    //     <div>
+    //         <div>
+    //         <div>
+    //             <h2>
+    //             Name:{ " " }
+    //             {this.props.firstName} { " " }
+    //             {this.props.lastName}
+    //             </h2>
+    //         </div>
+    //         <p>Age: {this.state.currentAge}</p>
+    //         <p>Hair Color: {this.props.hairColor}</p>
+    //         <button
+    //             onClick= {this.birthday}
+    //         >
+    //             birthday
+    //         </button>
+    //         </div>
+    //     </div>
+    //     );
+    // }
 }
 
 export default PersonCard
